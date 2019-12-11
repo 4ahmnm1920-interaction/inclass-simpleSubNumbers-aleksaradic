e87c7ba7d6ec7336a82e63952d84a1f28e234d5a
aleksaradic <aleksaradic01@gmail.com>
2019-12-11 11:27:50 +0100

    Code verschoenern

--- Assets/MyGame/Scripts/CumputeNumbers.cs
+++ Assets/MyGame/Scripts/CumputeNumbers.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class CumputeNumbers : MonoBehaviour
@@ -11,7 +9,10 @@ public class CumputeNumbers : MonoBehaviour
     public Button btnSubNumbers;
     public Button btnReset;
 
-    int index = 0;
+    void Start()
+    {
+        btnReset.interactable = false;
+    }
 
     public void ResetOnClick()
     {
